716150f76c888ed4085298b8476d6cb3418a27af
Utsav247-crypto <57640293+Utsav247-crypto@users.noreply.github.com>
2023-03-04 14:32:12 +0530

    Update final.py

--- final.py
+++ final.py
@@ -1,12 +1,1 @@
-from slack_sdk import WebClient
-
-client = WebClient(token='xoxb-4891217860754-4882528625543-lFT8ePlEa7rHssjeNwxZepaK')
-client.chat_postMessage(channel="random",text="My File")
-
-client.files_upload(
-        channels="random",
-        file='data.xlsx',
-        title='XLSX DATA',
-        filetype='xlsx'
-    )
-
+print(1)
